4bf291e29720cd1f46450704199a1ad451b9720c
alech97 <alech97@vt.edu>
2017-10-04 10:23:23 -0400

    Final draft

---
+++ script.js
@@ -0,0 +1,31 @@
+var devToolsOpen;
+
+var element = document.createElement('any');
+element.__defineGetter__('id', function() {
+    devToolsOpen = true;
+});
+
+setInterval(function() {
+	toggleDevTools(devToolsOpen);
+    devToolsOpen = false;
+    console.log(element);
+    console.clear();
+}, 500);
+
+function toggleDevTools(check) {
+	document.getElementById('devTools').style.display = (check) ? 'none' : 'block';
+    document.getElementById('container').style.width = (check) ? '100%' : '50%';
+}
+
+window.onscroll = function() {
+	if (document.scrollingElement.scrollTop === 0) {
+		document.getElementById('header').style.position = 'static';
+		document.getElementById('header').style.boxShadow = 'none';
+		document.getElementById('header').style.webkitBoxShadow = 'none';
+	}
+	else {
+		document.getElementById('header').style.position = 'sticky';
+		document.getElementById('header').style.boxShadow = '0px 2px 2px rgba(0, 0, 0, 0.28)';
+		document.getElementById('header').style.webkitBoxShadow = '0px 2px 2px rgba(0, 0, 0, 0.28)';
+	}
+};
